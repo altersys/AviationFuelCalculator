943826f842217a1c1f823f40ef1b614caa7501ff
raptor <neverguesswhat@gmail.com>
2020-11-18 14:39:58 +0300

    added Toast on any edit of density

--- app/src/main/java/com/example/aviationfuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/aviationfuelcalculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.aviationfuelcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<EditText>(R.id.editTextNumberDecimalDensity).addTextChangedListener {
+            // create a Toast with some text, to appear for a short time
+            val myToast = Toast.makeText(applicationContext , "Hello Toast!", Toast.LENGTH_SHORT)
+            // show the Toast
+            myToast.show()
+        }
     }
+
+
 }
